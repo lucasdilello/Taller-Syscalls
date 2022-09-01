9bf4e793467d88ce90719f106247c8db370fd63e
lucasdilello <lucasdilello@gmail.com>
2022-09-01 01:53:05 -0300

    hai

--- Taller de Syscalls-20220830/src/hai.c
+++ Taller de Syscalls-20220830/src/hai.c
@@ -7,7 +7,52 @@
 #include <stdlib.h>
 #include <errno.h>
 
+int counter = 0;
+
+void child(int sig) {
+	counter++;
+	printf("ya va\n");
+}
+
+void parent(int sig) {
+	int status;
+	if (wait(&status) < 0) { perror("waitpid"); exit(1); }
+	exit(0);
+}
+
+
+
 int main(int argc, char* argv[]) {
-  	// Completar
+  	pid_t child_id;
+
+	if (argc <= 1) {
+		//printf("Argumento invalido\n");
+		printf("ERROR argumento invalido");
+		exit(1);
+	}
+
+	child_id = fork();
+	if (child_id == -1) { perror("ERROR"); return 1; }
+	if (child_id == 0) {
+
+		while(counter < 5){
+			signal(SIGURG, child);
+		}
+		
+		kill(getppid(), SIGCHLD);
+		execvp(argv[1], argv+1);
+		perror("ERROR"); exit(1);
+
+	} else {
+		signal(SIGCHLD, parent);
+
+		while (1) {
+			sleep(1);
+			printf("sup!\n");
+			kill(child_id, SIGURG);
+		}
+		
+	}
 	return 0;
 }
+
