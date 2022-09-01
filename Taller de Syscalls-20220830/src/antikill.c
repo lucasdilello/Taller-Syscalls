976cfda25555fdbfce6b00e7d604137b043ea548
lucasdilello <lucasdilello@gmail.com>
2022-09-01 02:17:51 -0300

    antikill

--- Taller de Syscalls-20220830/src/antikill.c
+++ Taller de Syscalls-20220830/src/antikill.c
@@ -8,28 +8,54 @@
 #include <errno.h>
 
 int main(int argc, char* argv[]) {
-	int status;
-	pid_t child;
+	int status, sysno, rax, signal;
 
 	if (argc <= 1) {
-		fprintf(stderr, "Uso: %s comando [argumentos ...]\n", argv[0]);
+		fprintf(stderr, "Uso: %s commando [argumentos ...]\n", argv[0]);
 		exit(1);
 	}
 
 	/* Fork en dos procesos */
-	child = fork();
-	if (child == -1) { perror("ERROR fork"); return 1; }
+	pid_t child = fork();
 	if (child == 0) {
-		/* S'olo se ejecuta en el Hijo */
+		/* Solo se ejecuta en el hijo */
+		if (ptrace(PTRACE_TRACEME, 0, NULL, NULL)) {
+			perror("ERROR child ptrace(PTRACE_TRACEME, ...)");
+			exit(1);
+		}
 		execvp(argv[1], argv+1);
 		/* Si vuelve de exec() hubo un error */
-		perror("ERROR child exec(...)"); exit(1);
+		perror("ERROR child exec(...)");
+		exit(1);
 	} else {
-		/* S'olo se ejecuta en el Padre */
+		/* Solo se ejecuta en el padre */
 		while(1) {
-			if (wait(&status) < 0) { perror("waitpid"); break; }
+			if (wait(&status) < 0) { perror("wait"); break; }
 			if (WIFEXITED(status)) break; /* Proceso terminado */
+
+			sysno = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);//nro de syscall en 8 * ORIG_RAX
+			rax = ptrace(PTRACE_PEEKUSER, child, 8 * RAX, NULL); 
+			signal = ptrace(PTRACE_PEEKUSER, child, 8 * RSI, NULL);
+			if (rax == -ENOSYS) { 
+				if (sysno == SYS_kill ) { //chequeo si la syscall a ejecutar es kill
+					fprintf(stderr, "se envío la señal %i\n", signal);
+					if(signal == SIGKILL){
+						
+						ptrace(PTRACE_POKEUSER, child, 8*ORIG_RAX, SYS_exit);
+						ptrace(PTRACE_POKEUSER, child, 8*RDI, 1);
+						
+					break;
+					}
+				if(sysno == SYS_exit){
+					break;
+				}
+			
+					}
+			}
+
+			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		}
+		ptrace(PTRACE_DETACH, child, NULL, NULL);
 	}
 	return 0;
 }
